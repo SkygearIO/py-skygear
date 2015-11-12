0666a455259ebd5b223111b7a7caee391dd0f6ec
Kwok-kuen Cheung <cheungpat@oursky.com>
2015-11-12 17:56:35 +0800

    Run test case in skygear directory

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ classifiers = [
 class PyTest(TestCommand):
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = []
+        self.test_args = ['skygear']
         self.test_suite = True
     def run_tests(self):
         #import here, cause outside the eggs aren't loaded
