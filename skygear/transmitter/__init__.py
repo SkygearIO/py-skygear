dc2e7227a9ab260e10eb837bc9430b59a1a47c91
Rick Mak <rick.mak@gmail.com>
2017-03-24 18:38:23 +0800

    Update the mock interface for ZMQTransport
    refs #122

--- skygear/transmitter/__init__.py
+++ skygear/transmitter/__init__.py
@@ -23,7 +23,7 @@ except ImportError:
         """A dummy ZmqTransport class to raise a proper exception
         """
 
-        def __init__(self, addr, context=None, registry=None):
+        def __init__(self, addr, context=None, registry=None, threading=0):
             raise ImportError(
                 'zmq transport is not installed. '
                 'Please install via `pip install --upgrade skygear[zmq]`')
