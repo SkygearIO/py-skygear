220cd9fb135ac08582e499c4e9e0f1b3e8994d5b
Ho Wa <howa@oursky.com>
2017-11-30 11:00:17 +0800

    Update .travis.yml and deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,11 +15,11 @@ $MAKE docker-push DOCKER_REGISTRY=quay.io/
 if [ -n "$TRAVIS_TAG" ]; then
     $MAKE docker-push-version PUSH_DOCKER_TAG="${TRAVIS_TAG}"
     $MAKE docker-push-version DOCKER_REGISTRY=quay.io/ PUSH_DOCKER_TAG="${TRAVIS_TAG}"
-    generate-python-doc  --pwd
+    generate-python-doc  --pwd $PWD
     publish-doc --platform python --pwd $PWD  --doc-dir $PWD/docs/_build --bucket 'docs.skygear.io' --prefix '/py/reference' --version $TRAVIS_TAG --distribution-id E31J8XF8IPV2V
 else
     $MAKE docker-push-version PUSH_DOCKER_TAG="${TRAVIS_BRANCH/master/canary}"
     $MAKE docker-push-version DOCKER_REGISTRY=quay.io/ PUSH_DOCKER_TAG="${TRAVIS_BRANCH/master/canary}"
-    generate-python-doc  --pwd ./docs
+    generate-python-doc  --pwd $PWD
     publish-doc --platform python --pwd $PWD  --doc-dir $PWD/docs/_build --bucket 'docs.skygear.io' --prefix '/py/reference' --version $TRAVIS_BRANCH --distribution-id E31J8XF8IPV2V
 fi
