c4766f8d461729f72696de47742566b99cb4327a
Kwok-kuen Cheung <cheungpat@oursky.com>
2018-03-15 17:23:36 +0800

    Update bcrypt to 3.1.4
    The newer bcrypt package has binary wheel files available on pypi.

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ setup(
             'strict-rfc3339>=0.5',
             'requests',
             'websocket-client>=0.32.0',
-            'bcrypt==2.0.0',
+            'bcrypt==3.1.4',
             'ConfigArgParse>=0.12.0',
             'werkzeug>=0.11.0',
             'boto3>=1.4',
